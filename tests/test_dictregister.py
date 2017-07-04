812285c62c201a1a939eff5373cd5d2ba9acd027
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-04 07:39:03 +0100

    TEST test_mutable_sequence()

--- tests/test_dictregister.py
+++ tests/test_dictregister.py
@@ -1,5 +1,11 @@
+import collections
+
 from tdd import dictregister as dr
 
 
 def test_init():
     dr.DictRegister()
+
+
+def test_mutable_sequence():
+    assert isinstance(dr.DictRegister(), collections.MutableSequence)
