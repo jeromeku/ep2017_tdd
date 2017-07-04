8a8465789e5b9a6fba003c4847c18f5a6629e5fb
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-04 07:40:48 +0100

    TEST test_append_works()

--- tests/test_dictregister.py
+++ tests/test_dictregister.py
@@ -9,3 +9,12 @@ def test_init():
 
 def test_mutable_sequence():
     assert isinstance(dr.DictRegister(), collections.MutableSequence)
+
+
+def test_append_works():
+    d = dr.DictRegister()
+
+    d.append({'a': 1, 'b': 2})
+
+    assert len(d) == 1
+    assert d[0] == {'a': 1, 'b': 2}
