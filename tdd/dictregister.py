84ad9db9888e98a55c702974dce1a5b2d96e87d7
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-04 17:50:29 +0100

    CODE test_remove_keyword_value_from_multiple_values()

--- tdd/dictregister.py
+++ tdd/dictregister.py
@@ -52,7 +52,20 @@ class DictRegister(list):
     def kremove(self, key, value=None):
         for item in self:
             if value is None:
+                # Just pop the key if present,
+                # otherwise return None
+                # (shortcut to ignore the exception)
                 item.pop(key, None)
             else:
-                if item[key] == value:
-                    item.pop(key)
+                try:
+                    # Use the key as a set
+                    item[key].remove(value)
+                    # If the set contains a single element
+                    # just store the latter
+                    if len(item[key]) == 1:
+                        item[key] = item[key].pop()
+                except AttributeError:
+                    # This happens when the key is not a set
+                    # and shall be removed only if values match
+                    if item[key] == value:
+                        item.pop(key)
