b59f954e6332183c0a713d36c2c130ec853c6bbb
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-05 22:11:44 +0100

    TEST test_datawrapper_list_returns_correct_data() [refactoring]

--- tests/test_datawrapper.py
+++ tests/test_datawrapper.py
@@ -24,7 +24,7 @@ def test_datawrapper_list_calls_list_method(mock_restservice):
 
 @patch('tdd.datawrapper.RestService')
 def test_datawrapper_list_returns_correct_data(mock_restservice):
-    test_data = Mock()
+    test_data = [Mock()]
 
     mock_restservice().list.return_value = test_data
 
