3eb6dbd9f20d1d1861bbf11d066a0f304de26e07
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-05 20:06:23 +0100

    TEST test_datawrapper_list_filter_age()

--- tests/test_datawrapper.py
+++ tests/test_datawrapper.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from tdd import datawrapper as dw
 
@@ -24,6 +24,29 @@ def test_datawrapper_list_calls_list_method(mock_restservice):
 
 @patch('tdd.datawrapper.RestService')
 def test_datawrapper_list_returns_correct_data(mock_restservice):
+    test_data = Mock()
+
+    mock_restservice().list.return_value = test_data
+
+    w = dw.DataWrapper()
+
+    assert w.list() == test_data
+
+
+@patch('tdd.datawrapper.RestService')
+def test_datawrapper_get_returns_correct_data(mock_restservice):
+    test_data = Mock()
+
+    mock_restservice().get.return_value = test_data
+
+    w = dw.DataWrapper()
+
+    assert w.get(1) == test_data
+    mock_restservice().get.assert_called_with(1)
+
+
+@patch('tdd.datawrapper.RestService')
+def test_datawrapper_list_filter_age(mock_restservice):
     test_data = [
         {
             "id": 1,
@@ -45,29 +68,28 @@ def test_datawrapper_list_returns_correct_data(mock_restservice):
             "surname": "Fields",
             "age": 70,
             "salary": "£70472"
+        }
+    ]
+
+    expected_result = [
+        {
+            "id": 1,
+            "name": "Laith",
+            "surname": "Simmons",
+            "age": 68,
+            "salary": "£27888"
         },
+        {
+            "id": 3,
+            "name": "Garth",
+            "surname": "Fields",
+            "age": 70,
+            "salary": "£70472"
+        }
     ]
 
     mock_restservice().list.return_value = test_data
 
     w = dw.DataWrapper()
 
-    assert w.list() == test_data
-
-
-@patch('tdd.datawrapper.RestService')
-def test_datawrapper_get_returns_correct_data(mock_restservice):
-    test_data = {
-        "id": 1,
-        "name": "Laith",
-        "surname": "Simmons",
-        "age": 68,
-        "salary": "£27888"
-    }
-
-    mock_restservice().get.return_value = test_data
-
-    w = dw.DataWrapper()
-
-    assert w.get(1) == test_data
-    mock_restservice().get.assert_called_with(1)
+    assert w.list(age__gt=50) == expected_result
