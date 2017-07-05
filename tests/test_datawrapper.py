23c49b2971278fb42e2d16c4e9eebed0bdf6c2b0
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-05 19:50:33 +0100

    TEST test_datawrapper_list_returns_correct_data()

--- tests/test_datawrapper.py
+++ tests/test_datawrapper.py
@@ -20,3 +20,36 @@ def test_datawrapper_list_calls_list_method(mock_restservice):
     w.list()
 
     assert mock_restservice().list.called
+
+
+@patch('tdd.datawrapper.RestService')
+def test_datawrapper_list_returns_correct_data(mock_restservice):
+    test_data = [
+        {
+            "id": 1,
+            "name": "Laith",
+            "surname": "Simmons",
+            "age": 68,
+            "salary": "£27888"
+        },
+        {
+            "id": 2,
+            "name": "Mikayla",
+            "surname": "Henry",
+            "age": 49,
+            "salary": "£67137"
+        },
+        {
+            "id": 3,
+            "name": "Garth",
+            "surname": "Fields",
+            "age": 70,
+            "salary": "£70472"
+        },
+    ]
+
+    mock_restservice().list.return_value = test_data
+
+    w = dw.DataWrapper()
+
+    assert w.list() == test_data
