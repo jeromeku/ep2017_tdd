fdc0e3823ba493bcd994a0c0248c23839f7fa8df
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-05 07:54:12 +0100

    TEST test_datawrapper_list_calls_list_method()

--- tests/test_datawrapper.py
+++ tests/test_datawrapper.py
@@ -12,3 +12,11 @@ def test_datawrapper_initializes_restservice(mock_restservice):
     dw.DataWrapper()
 
     assert mock_restservice.called
+
+
+@patch('tdd.datawrapper.RestService')
+def test_datawrapper_list_calls_list_method(mock_restservice):
+    w = dw.DataWrapper()
+    w.list()
+
+    assert mock_restservice().list.called
