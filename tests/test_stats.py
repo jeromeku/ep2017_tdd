b7b49a5440079180cf6f19306edb6e699f4cec8c
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-08 08:49:58 +0100

    TEST test_json() [refactored]

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -39,27 +39,27 @@ def test_init_with_data(test_data):
     assert len(ds.data) == 3
 
 
-# def test_json(test_data):
-#     ds = DataStats()
+def test_json(test_data):
+    ds = DataStats(test_data)
 
-#     assert ds.stats(test_data, 20, 20000) == json.dumps(
-#         {
-#             'avg_age': 62,
-#             'avg_salary': 55165,
-#             'avg_yearly_increase': 837,
-#             'max_salary': [{
-#                 "id": 3,
-#                 "name": "Garth",
-#                 "surname": "Fields",
-#                 "age": 70,
-#                 "salary": "£70472"
-#             }],
-#             'min_salary': [{
-#                 "id": 1,
-#                 "name": "Laith",
-#                 "surname": "Simmons",
-#                 "age": 68,
-#                 "salary": "£27888"
-#             }]
-#         }
-#     )
+    assert ds.stats(20, 20000) == json.dumps(
+        {
+            'avg_age': 62,
+            'avg_salary': 55165,
+            'avg_yearly_increase': 837,
+            'max_salary': [{
+                "id": 3,
+                "name": "Garth",
+                "surname": "Fields",
+                "age": 70,
+                "salary": "£70472"
+            }],
+            'min_salary': [{
+                "id": 1,
+                "name": "Laith",
+                "surname": "Simmons",
+                "age": 68,
+                "salary": "£27888"
+            }]
+        }
+    )
